6ce88e0e7142db24de84a438eb3a782eb4e56a4b
Antonio Borondo <antonioborondo@outlook.com>
2021-08-22 18:59:48 +0200

    Add function to get all filenames from directory sorted

--- renamer/renamer.cpp
+++ renamer/renamer.cpp
@@ -3,13 +3,51 @@
 
 #include "renamer.h"
 
-#include <filesystem>
+#include <natural_sort/natural_sort.hpp>
 
-using namespace std;
+#include <filesystem>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+std::vector<fs::path> get_filenames_from_directory(const fs::path& directory)
+{
+    std::vector<fs::path> filenames;
+
+    if (fs::exists(directory) && fs::is_directory(directory))
+    {
+        for (fs::directory_iterator directory_iterator{ directory }; fs::directory_iterator{} != directory_iterator; directory_iterator++)
+        {
+            if (fs::is_regular_file(directory_iterator->status()))
+            {
+                filenames.push_back(directory_iterator->path());
+            }
+        }
+    }
+
+    std::sort(filenames.begin(), filenames.end(), [](const fs::path& filename_1, const fs::path& filename_2)
+        {
+            return SI::natural::compare<std::wstring>(filename_1.filename().wstring(), filename_2.filename().wstring());
+        });
+
+    return filenames;
+}
+
 
 int main()
 {
-	cout << std::filesystem::current_path() << endl;
+    const auto directory{std::filesystem::current_path()};
+
+    std::cout << "Directory: " << directory << std::endl;
+
+    const auto filenames{ get_filenames_from_directory(directory)};
+
+    std::cout << "Filenames:" << std::endl;
+
+    for (const auto& filename : filenames)
+    {
+        std::cout << filename << std::endl;
+    }
 	
 	return 0;
 }
