785ee9434815c86f2cbfa19f6eac51fcf972897b
Antonio Borondo <antonioborondo@outlook.com>
2021-08-22 18:41:41 +0200

    Display current path

--- renamer/renamer.cpp
+++ renamer/renamer.cpp
@@ -3,10 +3,13 @@
 
 #include "renamer.h"
 
+#include <filesystem>
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << std::filesystem::current_path() << endl;
+	
 	return 0;
 }
