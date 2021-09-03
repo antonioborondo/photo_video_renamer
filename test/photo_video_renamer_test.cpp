537bc79863297e1bcad0336484cad7935e17cdbb
Antonio Borondo <antonioborondo@outlook.com>
2021-09-03 19:05:01 +0200

    Rename tests

--- test/photo_video_renamer_test.cpp
+++ test/photo_video_renamer_test.cpp
@@ -49,20 +49,20 @@ public:
     }
 };
 
-TEST(DirectoryExists, NonExistingDirectoryDoesNotExists)
+TEST(DirectoryExistsTest, NonExistingDirectoryDoesNotExists)
 {
     const std::filesystem::path non_existing_directory{std::tmpnam(nullptr)};
     ASSERT_FALSE(directory_exists(non_existing_directory));
 }
 
-TEST(DirectoryExists, ExistingFileIsNotDirectory)
+TEST(DirectoryExistsTest, ExistingFileIsNotDirectory)
 {
     Directory_wrapper parent_directory;
     File_wrapper file{parent_directory, "test.txt"};
     ASSERT_FALSE(directory_exists(file.path()));
 }
 
-TEST(DirectoryExists, ExistingDirectoryExists)
+TEST(DirectoryExistsTest, ExistingDirectoryExists)
 {
     Directory_wrapper directory;
     ASSERT_TRUE(directory_exists(directory.path()));
