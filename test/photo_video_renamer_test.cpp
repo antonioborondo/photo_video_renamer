dfb2e85c52dbc8a5035e773fdaed74c31959c4bd
Antonio Borondo <antonioborondo@outlook.com>
2021-09-03 17:46:33 +0200

    Add tests for directory_exists function

--- test/photo_video_renamer_test.cpp
+++ test/photo_video_renamer_test.cpp
@@ -2,7 +2,37 @@
 
 #include <gtest/gtest.h>
 
-TEST(test, test)
+#include <filesystem>
+
+class Directory_wrapper : public ::testing::Test
 {
-    ASSERT_TRUE(true);
+protected:
+    std::filesystem::path m_directory;
+
+    Directory_wrapper()
+    {
+        m_directory = std::tmpnam(nullptr);
+
+        std::filesystem::create_directory(m_directory);
+    }
+
+    ~Directory_wrapper()
+    {
+        std::filesystem::remove_all(m_directory);
+    }
+};
+
+class Directory_exists_test : public Directory_wrapper
+{
+};
+
+TEST_F(Directory_exists_test, NonExistingDirectoryDoesNotExists)
+{
+    const std::filesystem::path non_existing_directory{std::tmpnam(nullptr)};
+    ASSERT_FALSE(directory_exists(non_existing_directory));
+}
+
+TEST_F(Directory_exists_test, ExistingDirectoryExists)
+{
+    ASSERT_TRUE(directory_exists(m_directory));
 }
