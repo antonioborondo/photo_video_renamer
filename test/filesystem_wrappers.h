ed9488e96685e2fddde7c076ebe3e630ad8b11da
Antonio Borondo <antonioborondo@outlook.com>
2021-09-06 20:08:12 +0200

    Move filesystem wrappers into their own header file

---
+++ test/filesystem_wrappers.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+
+class Filesystem_wrapper
+{
+protected:
+    std::filesystem::path m_path;
+
+    Filesystem_wrapper(){}
+
+public:
+    ~Filesystem_wrapper()
+    {
+        std::filesystem::remove_all(m_path);
+    }
+
+    const std::filesystem::path path() const
+    {
+        return m_path;
+    }
+};
+
+class Directory_wrapper : public Filesystem_wrapper
+{
+public:
+    Directory_wrapper()
+    : Filesystem_wrapper{}
+    {
+        m_path = std::tmpnam(nullptr);
+        std::filesystem::create_directory(m_path);
+    }
+};
+
+class File_wrapper : public Filesystem_wrapper
+{
+public:
+    File_wrapper(const Directory_wrapper& parent_directory, const std::filesystem::path& filename)
+    : Filesystem_wrapper{}
+    {
+        m_path = parent_directory.path();
+        m_path /= filename;
+        std::ofstream file{m_path};
+    }
+};
--- test/photo_video_renamer_test.cpp
+++ test/photo_video_renamer_test.cpp
@@ -1,53 +1,11 @@
 #include "photo_video_renamer.h"
 
+#include "filesystem_wrappers.h"
+
 #include <gtest/gtest.h>
 
+#include <cstdio>
 #include <filesystem>
-#include <fstream>
-
-class Filesystem_wrapper
-{
-protected:
-    std::filesystem::path m_path;
-
-    Filesystem_wrapper()
-    {
-    }
-
-public:
-    ~Filesystem_wrapper()
-    {
-        std::filesystem::remove_all(m_path);
-    }
-
-    const std::filesystem::path path() const
-    {
-        return m_path;
-    }
-};
-
-class Directory_wrapper : public Filesystem_wrapper
-{
-public:
-    Directory_wrapper()
-    : Filesystem_wrapper{}
-    {
-        m_path = std::tmpnam(nullptr);
-        std::filesystem::create_directory(m_path);
-    }
-};
-
-class File_wrapper : public Filesystem_wrapper
-{
-public:
-    File_wrapper(const Directory_wrapper& parent_directory, const std::filesystem::path& filename)
-    : Filesystem_wrapper{}
-    {
-        m_path = parent_directory.path();
-        m_path /= filename;
-        std::ofstream file{m_path};
-    }
-};
 
 TEST(DirectoryExists, NonExistingDirectoryDoesNotExists)
 {
