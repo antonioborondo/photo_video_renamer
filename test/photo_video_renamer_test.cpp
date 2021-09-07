a856c79fa46336185e387dd5555ff0b58e32cd8e
Antonio Borondo <antonioborondo@outlook.com>
2021-09-07 18:37:41 +0200

    Add more tests

--- test/photo_video_renamer_test.cpp
+++ test/photo_video_renamer_test.cpp
@@ -51,10 +51,41 @@ TEST(FilenameIsPhotoOrVideo, NonPhotoOrVideoFilenamesInLowerAndUpperCase)
     ASSERT_FALSE(filename_is_photo_or_video("TEST.DOC"));
 }
 
-TEST(GetFilenamesFromDirectory, NoFilenamesAreReturned)
+TEST(GetFilenamesFromDirectory, NoFilenamesAreReturnedIfDirectoryIsEmpty)
 {
     Directory_wrapper empty_directory;
     const auto filenames{get_filenames_from_directory(empty_directory.path())};
 
-    EXPECT_THAT(filenames, ::testing::IsEmpty());
+    ASSERT_THAT(filenames, testing::IsEmpty());
+}
+
+TEST(GetFilenamesFromDirectory, OnlyPhotosAndVideosAreReturned)
+{
+    Directory_wrapper parent_directory;
+    File_wrapper file_1{parent_directory, "1.jpg"};
+    File_wrapper file_2{parent_directory, "2.jpeg"};
+    File_wrapper file_3{parent_directory, "3.heic"};
+    File_wrapper file_4{parent_directory, "4.mov"};
+    File_wrapper file_5{parent_directory, "5.mp4"};
+    File_wrapper file_6{parent_directory, "6.txt"};
+    File_wrapper file_7{parent_directory, "7.pdf"};
+    File_wrapper file_8{parent_directory, "8.doc"};
+
+    const auto filenames{get_filenames_from_directory(parent_directory.path())};
+
+    ASSERT_THAT(filenames, testing::UnorderedElementsAre(file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path()));
+}
+
+TEST(GetFilenamesFromDirectory, FilenamesReturnedAreSorted)
+{
+    Directory_wrapper parent_directory;
+    File_wrapper file_1{parent_directory, "1.jpg"};
+    File_wrapper file_2{parent_directory, "2.jpeg"};
+    File_wrapper file_3{parent_directory, "3.heic"};
+    File_wrapper file_4{parent_directory, "4.mov"};
+    File_wrapper file_5{parent_directory, "5.mp4"};
+
+    const auto filenames{get_filenames_from_directory(parent_directory.path())};
+
+    ASSERT_THAT(filenames, testing::ElementsAre(file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path()));
 }
