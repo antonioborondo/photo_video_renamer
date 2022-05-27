15793df14b50e77abd32c569cc20e62f5f2a4b34
Antonio Borondo <antonioborondo@outlook.com>
2022-05-27 20:43:27 +0200

    Support .avi extension

--- test/photo_video_renamer_test.cpp
+++ test/photo_video_renamer_test.cpp
@@ -34,6 +34,8 @@ TEST(DirectoryExists, ExistingDirectoryExists)
 
 TEST(FilenameIsPhotoOrVideo, PhotoOrVideoFilenamesInLowerAndUpperCase)
 {
+    ASSERT_TRUE(filename_is_photo_or_video("file.avi"));
+    ASSERT_TRUE(filename_is_photo_or_video("file.AVI"));
     ASSERT_TRUE(filename_is_photo_or_video("file.heic"));
     ASSERT_TRUE(filename_is_photo_or_video("file.HEIC"));
     ASSERT_TRUE(filename_is_photo_or_video("file.jpeg"));
@@ -69,6 +71,7 @@ TEST(GetFilenamesFromDirectory, NoFilenamesAreReturnedIfDirectoryIsEmpty)
 TEST(GetFilenamesFromDirectory, OnlyPhotosAndVideosAreReturned)
 {
     Directory_wrapper parent_directory;
+    File_wrapper file_0{parent_directory, "file_0.avi"};
     File_wrapper file_1{parent_directory, "file_1.heic"};
     File_wrapper file_2{parent_directory, "file_2.jpeg"};
     File_wrapper file_3{parent_directory, "file_3.jpg"};
@@ -81,12 +84,13 @@ TEST(GetFilenamesFromDirectory, OnlyPhotosAndVideosAreReturned)
 
     const auto filenames{get_filenames_from_directory(parent_directory.path())};
 
-    ASSERT_THAT(filenames, testing::UnorderedElementsAre(file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path(), file_6.path()));
+    ASSERT_THAT(filenames, testing::UnorderedElementsAre(file_0.path(), file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path(), file_6.path()));
 }
 
 TEST(GetFilenamesFromDirectory, FilenamesReturnedAreSorted)
 {
     Directory_wrapper parent_directory;
+    File_wrapper file_0{parent_directory, "file_0.avi"};
     File_wrapper file_1{parent_directory, "file_1.heic"};
     File_wrapper file_2{parent_directory, "file_2.jpeg"};
     File_wrapper file_3{parent_directory, "file_3.jpg"};
@@ -96,7 +100,7 @@ TEST(GetFilenamesFromDirectory, FilenamesReturnedAreSorted)
 
     const auto filenames{get_filenames_from_directory(parent_directory.path())};
 
-    ASSERT_THAT(filenames, testing::ElementsAre(file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path(), file_6.path()));
+    ASSERT_THAT(filenames, testing::ElementsAre(file_0.path(), file_1.path(), file_2.path(), file_3.path(), file_4.path(), file_5.path(), file_6.path()));
 }
 
 TEST(GenerateNewFilenames, NumberOfInputFilenamesIsEqualToNumberOfGeneratedFilenames)
