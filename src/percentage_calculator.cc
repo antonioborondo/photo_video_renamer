f13543b44063487ae7d013c8c8443ba4b97582ce
Antonio Borondo <antonioborondo@outlook.com>
2023-03-10 16:26:32 +0100

    Remove percentage calculator

--- src/percentage_calculator.cc
+++
@@ -1,9 +0,0 @@
-#include "percentage_calculator.h"
-
-#include <fmt/format.h>
-
-std::string CalculatePercentage(size_t renamed, size_t total)
-{
-    const auto percentage{0 < total ? (renamed * 100) / total : 0};
-    return fmt::format("{0}%", percentage);
-}
--- src/percentage_calculator.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string CalculatePercentage(size_t renamed, size_t total);
